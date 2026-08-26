d25cb06e907d2ab4e799092f8347e5d810efb421
phillip <flipking037@gmail.com>
2026-08-26 08:16:53 +0300

    Create LoginForm

---
+++ lib/views/auth/views/widgets/login_form.dart
@@ -0,0 +1,35 @@
+import 'package:animoo_app/core/utils/app_strings.dart';
+import 'package:animoo_app/core/utils/app_style.dart';
+import 'package:animoo_app/core/widgets/custom_text_form_field.dart';
+import 'package:flutter/material.dart';
+
+class LoginForm extends StatelessWidget {
+  const LoginForm({super.key, required this.formKey});
+
+ final GlobalKey<FormState> formKey;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      child: Column(
+        children: [
+          Text(AppStrings.email, style: AppStyle.labelStyle),
+          SizedBox(height: 6),
+          CustomTextFormField(hint: AppStrings.enterYourEmailAddress),
+          SizedBox(height: 16),
+          Text(AppStrings.password, style: AppStyle.labelStyle),
+          SizedBox(height: 6),
+          CustomTextFormField(
+            obscureText: true,
+            hint: AppStrings.enterYourPassword,
+            suffix: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.remove_red_eye),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/views/auth/views/widgets/login_view_body.dart
+++ lib/views/auth/views/widgets/login_view_body.dart
@@ -1,9 +1,8 @@
 import 'package:animoo_app/core/utils/app_images.dart';
-import 'package:animoo_app/core/utils/app_strings.dart';
-import 'package:animoo_app/core/utils/app_style.dart';
+
 import 'package:animoo_app/core/widgets/custom_elevated_button.dart';
-import 'package:animoo_app/core/widgets/custom_text_form_field.dart';
 import 'package:animoo_app/views/auth/views/widgets/forget_password.dart';
+import 'package:animoo_app/views/auth/views/widgets/login_form.dart';
 import 'package:animoo_app/views/auth/views/widgets/title_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -22,20 +21,7 @@ class LoginViewBody extends StatelessWidget {
             Center(child: Image.asset(AppImages.logo, width: 72)),
             SizedBox(height: 9.15),
             TitleAuth(),
-            Text(AppStrings.email, style: AppStyle.labelStyle),
-            SizedBox(height: 6),
-            CustomTextFormField(hint: AppStrings.enterYourEmailAddress),
-            SizedBox(height: 16),
-            Text(AppStrings.password, style: AppStyle.labelStyle),
-            SizedBox(height: 6),
-            CustomTextFormField(
-              obscureText: true,
-              hint: AppStrings.enterYourPassword,
-              suffix: IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.remove_red_eye),
-              ),
-            ),
+            LoginForm(formKey: GlobalKey<FormState>()),
             ForgetPassword(),
             SizedBox(height: 31),
             CustomElevatedButton(),
