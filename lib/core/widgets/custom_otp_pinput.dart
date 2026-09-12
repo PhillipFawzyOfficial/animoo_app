272110e023c5ea26ff48899d22805d4e81b82d91
phillip <flipking037@gmail.com>
2026-09-12 17:17:36 +0300

    Create CustomOtpPinput Use Pinput

---
+++ lib/core/widgets/custom_otp_pinput.dart
@@ -0,0 +1,42 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:pinput/pinput.dart';
+
+class CustomOtpPinput extends StatelessWidget {
+  const CustomOtpPinput({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Pinput(
+          defaultPinTheme: PinTheme(
+            // 72 = 18 * 4 (separatorBuilder - Number SizedBox)
+            width: (MediaQuery.of(context).size.width - 72) / 5,
+            height: (MediaQuery.of(context).size.width - 72) / 5,
+            textStyle: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(12),
+              border: Border.all(color: const Color(0xffD9D9D9), width: 1),
+            ),
+          ),
+          length: 5,
+          separatorBuilder: (index) => const SizedBox(width: 18),
+          keyboardType: TextInputType.number,
+          onCompleted: (value) {
+            log(value);
+          },
+          onTapOutside: (event) {
+            FocusScope.of(context).unfocus();
+          },
+        ),
+      ],
+    );
+  }
+}
--- lib/views/auth/views/widgets/otp_view_body.dart
+++ lib/views/auth/views/widgets/otp_view_body.dart
@@ -1,3 +1,6 @@
+import 'package:animoo_app/core/utils/app_strings.dart';
+import 'package:animoo_app/core/utils/app_style.dart';
+import 'package:animoo_app/core/widgets/custom_otp_pinput.dart';
 import 'package:flutter/material.dart';
 
 class OtpViewBody extends StatelessWidget {
@@ -5,6 +8,26 @@ class OtpViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 18),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(AppStrings.otpVerfication, style: AppStyle.appBarTitle),
+            SizedBox(height: 6),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4),
+              child: Text(
+                AppStrings.pleaseEnterThe5DigitCodeSentYourPhoneNumber,
+                style: AppStyle.subtitleAuthStyle,
+              ),
+            ),
+            SizedBox(height: 54),
+            CustomOtpPinput(),
+          ],
+        ),
+      ),
+    );
   }
 }
