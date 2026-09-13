4f86d26beec83421f6a7b35b32709b19a6f5d803
phillip <flipking037@gmail.com>
2026-09-13 12:49:21 +0300

    Finsh CreateNewPassword

--- lib/views/auth/views/widgets/create_new_password_body.dart
+++ lib/views/auth/views/widgets/create_new_password_body.dart
@@ -1,3 +1,8 @@
+import 'package:animoo_app/core/utils/app_strings.dart';
+import 'package:animoo_app/core/utils/app_style.dart';
+import 'package:animoo_app/core/widgets/custom_elevated_button.dart';
+import 'package:animoo_app/views/auth/views/widgets/label_text_field_password.dart';
+import 'package:animoo_app/views/auth/views/widgets/password_rules.dart';
 import 'package:flutter/material.dart';
 
 class CreateNewPasswordBody extends StatelessWidget {
@@ -5,6 +10,36 @@ class CreateNewPasswordBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: []);
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 18),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Text(AppStrings.otpVerfication, style: AppStyle.appBarTitle),
+            LabelTextFieldPassword(
+              title: AppStrings.newPassword,
+              hint: AppStrings.enterYourNewPassword,
+              obscureText: true,
+              onPressed: () {},
+              controller: TextEditingController(),
+            ),
+            SizedBox(height: 8),
+            PasswordRules(),
+            SizedBox(height: 18),
+            LabelTextFieldPassword(
+              title: AppStrings.confirmPassword,
+              hint: AppStrings.confirmPassword,
+              obscureText: true,
+              onPressed: () {},
+              controller: TextEditingController(),
+            ),
+            SizedBox(height: 82),
+
+            CustomElevatedButton(text: 'Submit', onPressed: () {}),
+            SizedBox(height: 82),
+          ],
+        ),
+      ),
+    );
   }
 }
